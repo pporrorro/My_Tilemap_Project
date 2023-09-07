8675144f79dbe30dacace7ec99112cd99f9e9a6d
pporrorro <hiimhoihoi@gmail.com>
2023-09-07 12:24:23 +0900

    commit

---
+++ Assets/Scripts/Entities/TopDownMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownCharacterController _controller;
+
+    private Vector2 _movementDirection = Vector2.zero;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownCharacterController>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(_movementDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _movementDirection = direction;
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        _rigidbody.velocity = direction;
+    }
+}
